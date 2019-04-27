0065e6ef8826680218d442db32d7854d65c03835
Prakash Shettigar -T (pshettig - EXPERIS IT PRIVATE LIMITED at Cisco) <pshettig@cisco.com>
2019-04-27 13:41:44 +0530

    static file path fix

--- networkproject/settings.py
+++ networkproject/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/usr/src/app/static/',
+]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
